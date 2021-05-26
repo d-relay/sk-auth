a4806c8d8c6745fc9a1d41e4ea1a863c4d28db9a
x-ror <16686943+x-ror@users.noreply.github.com>
2021-05-26 13:33:45 +0300

    move svg define to js file

---
+++ app/src/lib/icons/SVG.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let icon;
+  export let width = 24;
+  export let height = 24;
+</script>
+
+<svg
+  fill="currentColor"
+  {width}
+  {height}
+  viewBox={icon.viewBox}
+  aria-labelledby={icon.name}
+  role="presentation"
+  class={icon.class}
+>{@html icon.src}</svg>
